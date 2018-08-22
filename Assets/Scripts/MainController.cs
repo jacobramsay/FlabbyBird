6ecd7c391e4f9f53ab04fbc14066915349553264
Jacob Ramsay <ramsayjacob@gmail.com>
2018-08-22 15:27:04 -0400

    Configured the project.

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LoadGame();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private static bool IsSceneLoaded(string name)
+    {
+        for (var i = 0; i < SceneManager.sceneCount; i++)
+            if (SceneManager.GetSceneAt(i).name == name) return true;
+
+        return false;
+    }
+
+    private static void LoadGame()
+    {
+        if (!IsSceneLoaded(Scenes.Game))
+        {
+            SceneManager.LoadSceneAsync(Scenes.Game, LoadSceneMode.Additive);
+        }
+
+        //La scène active est celle dans laquelle les GameObjects sont créés au runtime.
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(Scenes.Game));
+    }
+}
